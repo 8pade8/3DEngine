919109202b62e9ab564b5896db6eabe5cafeb8cd
8pade8 <8pade8@gmail.com>
2019-08-07 11:13:18 +0300

    Проведен рефакторинг класса

--- src/net/spades/Vector2D.java
+++ src/net/spades/Vector2D.java
@@ -6,46 +6,52 @@ import java.util.Map;
  * Created by stanislav on 22.06.16.
  */
 public class Vector2D {
-    double i;
-    double j;
+    private float i;
+    private float j;
 
-    public Vector2D(double i, double j)
+    Vector2D(float i, float j)
     {
         this.i = i;
         this.j = j;
     }
 
-    public Vector2D(Point2D A, Point2D B) {
-        this.i = B.x-A.x;
-        this.j = B.y-A.y;
+    public float getI() {
+        return i;
     }
 
-    public double Dimension()
-    {   double x = Math.sqrt(i*i+j*j);
-        return x;
+    public float getJ() {
+        return j;
     }
 
-    public double ScalarMultiply(Vector2D v)
+    Vector2D(Point2D A, Point2D B) {
+        this.i = B.getX()-A.getX();
+        this.j = B.getY()-A.getY();
+    }
+
+    float Dimension() {
+        return (float) Math.sqrt(i*i+j*j);
+    }
+
+    float ScalarMultiply(Vector2D v)
     {
-        double x = i*v.i+j*v.j;
-        return x;
+        return i*v.i+j*v.j;
     }
 
-    public Double cosAngle(Vector2D v) {
+    float cosAngle(Vector2D v) {
         return ScalarMultiply(v)/(Dimension()*v.Dimension());
     }
 
-    public Vector2D addVector(Vector2D v)
+    Vector2D addVector(Vector2D v)
     {
         return new Vector2D(i+v.i,j+v.j);
     }
 
-    public double Multiply(Vector2D v)
+    float Multiply(Vector2D v)
     {
         return i*v.j-j*v.i;
     }
 
-    public Vector2D Multiply(double d)
+    Vector2D Multiply(float d)
     {
         return new Vector2D(i*d,j*d);
     }
