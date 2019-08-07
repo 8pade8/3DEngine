0146228d0b1bd555ec681739cadf3e10ebab8359
8pade8 <8pade8@gmail.com>
2019-08-07 11:04:28 +0300

    Проведен рефакторинг класса

--- src/net/spades/Point2D.java
+++ src/net/spades/Point2D.java
@@ -4,16 +4,30 @@ package net.spades;
  * Created by ptmkpd on 17.06.16.
  */
 public class Point2D {
-    float x, y;
+    private float x, y;
 
-    public Point2D(float x, float y)
-    {
+    Point2D(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
-    public boolean isEquals(Point2D p)
-    {
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass() != Point2D.class) {
+            return false;
+        }
+        Point2D p = (Point2D) obj;
         return x == p.x & y == p.y;
     }
 }
